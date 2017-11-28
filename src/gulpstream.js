27c26165d54fda1e15f7bfd8e0ced32e2778f0f8
Jason Lenoble <jason.lenoble@gmail.com>
2017-11-28 05:28:49 +0100

    Extended GulpStream with mode and streamer prop
    Mode determines which Streamer is active
    (Streamer methods have still to be overridden properly)

--- src/gulpstream.js
+++ src/gulpstream.js
@@ -10,6 +10,10 @@ const uneq = unequiv({
   }],
 });
 
+const _mode = Symbol();
+const _streamer = Symbol();
+const _streamers = Symbol();
+
 const GulpStream = PolytonFactory( // eslint-disable-line new-cap
   Streamer, ['literal', 'object', {
     type: 'object',
@@ -18,7 +22,7 @@ const GulpStream = PolytonFactory( // eslint-disable-line new-cap
     type: 'object',
     optional: true,
   }], ['unique', 'unordered'], {
-    preprocess: function (args) {
+    preprocess (args) {
       let _glob;
       let _pipe;
       let _dest;
@@ -65,6 +69,59 @@ const GulpStream = PolytonFactory( // eslint-disable-line new-cap
       }
 
       return _args;
+    },
+
+    properties: {
+      [_mode]: {
+        value: 'default',
+        writable: true,
+      },
+
+      [_streamer]: {
+        value: null,
+        writable: true,
+      },
+
+      [_streamers]: {
+        value: {},
+      },
+
+      mode: {
+        get () {
+          return this[_mode];
+        },
+      },
+
+      streamer: {
+        get () {
+          return this[_streamer];
+        },
+      },
+    },
+
+    postprocess (instance, args) {
+      instance.forEach(p => {
+        instance[_streamers][p.mode] = p; // eslint-disable-line no-param-reassign, max-len
+      });
+      instance[_streamer] = instance[_streamers][instance[_mode]]; // eslint-disable-line no-param-reassign, max-len
+      return instance;
+    },
+
+    extend: {
+      setMode (mode) {
+        if (typeof mode === 'string') {
+          const strm = this[_streamers][mode];
+
+          if (strm) {
+            this[_mode] = mode;
+            this[_streamer] = strm;
+          } else {
+            throw new Error(`Mode ${mode} is undefined`);
+          }
+        } else {
+          throw new TypeError('Mode must be set with a string');
+        }
+      },
     },
   });
 
---
+++ test/mode.test.js
@@ -0,0 +1,54 @@
+import GulpGlob from 'gulpglob';
+import GulpDest from 'gulpdest';
+import PolyPipe from 'polypipe';
+import GulpStream from '../src/gulpstream';
+import {validGlobs, validDests} from './helpers';
+import {argsAsListsOfPlugins} from './pipe-helpers';
+import {expect} from 'chai';
+
+describe(`Testing mode switch for GulpStream`, function () {
+  const [glob1] = validGlobs();
+  const [dest1, dest2] = validDests();
+  const [plugin1, plugin2, plugin3] = argsAsListsOfPlugins;
+
+  const o1 = ['default', new GulpGlob(glob1), new PolyPipe(...plugin1.values),
+    new GulpDest(dest1)];
+  const o2 = ['other1', new GulpGlob(glob1), new PolyPipe(...plugin2.values),
+    new GulpDest(dest2)];
+  const o3 = ['other2', new GulpGlob(glob1), new PolyPipe(...plugin3.values),
+    new GulpDest(dest2)];
+  const o4 = ['other3', new GulpGlob(glob1), new PolyPipe(...plugin3.values),
+    new GulpDest(dest2)];
+
+  it(`Checking streamers don't already exist from another test`, function () {
+    expect(GulpStream.get(o1, o2, o3)).to.be.undefined;
+    expect(GulpStream.get(o1, o2, o4)).to.be.undefined;
+  });
+
+  it(`On init, mode is 'default'`, function () {
+    const gs = new GulpStream(o1, o2, o3);
+    expect(gs.mode).to.equal('default');
+  });
+
+  it(`On init, streamer is set to default streamer`, function () {
+    const gs = GulpStream.get(o1, o2, o3);
+    expect(gs.streamer.mode).to.equal('default');
+  });
+
+  it(`Changing mode to 'other1' changes streamer to 'other1' too`, function () {
+    const gs = GulpStream.get(o1, o2, o3);
+
+    expect(gs.mode).to.equal('default');
+    expect(gs.streamer.mode).to.equal('default');
+
+    gs.setMode('other1');
+
+    expect(gs.mode).to.equal('other1');
+    expect(gs.streamer.mode).to.equal('other1');
+
+    gs.setMode('default');
+
+    expect(gs.mode).to.equal('default');
+    expect(gs.streamer.mode).to.equal('default');
+  });
+});
